acb592eeba020566882625d80c0548378b0d847a
Larissa Simões <larissacarneiro2@hotmail.com>
2023-09-28 11:53:25 -0300

    Implementação da classe GerenciamentoVotacao com atributos

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,42 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/**
+ * Classe que representa o gerenciamento de votação.
+ */
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  /**
+   * Construtor da classe GerenciamentoVotacao.
+   */
+  public GerenciamentoVotacao() {
+    pessoasCandidatas = new ArrayList<>();
+    pessoasEleitoras = new ArrayList<>();
+    cpfsComputados = new ArrayList<>();
+  }
+
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
