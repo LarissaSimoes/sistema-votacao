8c5cf207db0e0d26ee96d52d012bd37c5c0c253b
Larissa Simões <larissacarneiro2@hotmail.com>
2023-09-28 11:37:23 -0300

    Implementação da classe pessoa eleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,22 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * Classe que representa uma pessoa candidata.
+ */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    this.setNome(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 
 }
